0197406ec3d36efc6bb2a20128c6d59e67be28cb
Christophe-Junier <christophejunier@gmail.com>
2016-12-07 16:38:54 +0100

    connexion en nfc

---
+++ ConnexionNFC.py
@@ -0,0 +1,19 @@
+#!/Python-3.5.2/
+#-*-coding:Utf-8 -*
+
+# initialisation du raspberry on devra integrer les differentes biblitothèque requise ici
+
+
+
+# programme
+tant que le liens nfc n'est pas fait
+{
+    attente de 0,5 seconde
+    Je reessais de faire le liens
+
+}
+#le liens est donc fait pour la suite
+ID = Je recupere l'id du badge et la stock dans la variable id badge
+#class donée a mettre en dessous
+ID_Badge = nouvelle donnée(ID)
+ID_Badge->Envoie_donnée(ID, destination)
