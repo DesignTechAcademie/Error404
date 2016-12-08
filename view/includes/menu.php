89372c461a9068f94c8e47c2a1ae1f603f1d4277
kevin <kev.landry>
2016-12-08 16:18:14 +0100

    ajout du site

---
+++ view/includes/menu.php
@@ -0,0 +1,15 @@
+<header>
+  <h1>
+    <a href="main_page.html"><img src="view/img/logo.png"></a>
+  </h1>
+  <nav class="sidebar">
+    <ul>
+      <li><a href="#">Dashboard</a></li>
+      <li><a href="#">Gestion membres</a></li>
+      <li><a href="#">Statistiques</a></li>
+      <li><a href="#">Calendrier</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+  <p id="copyright">© Groupe Error 404</p>
+</header>
---
+++ view/indexView.php
@@ -0,0 +1,8 @@
+<main>
+    <div id="row1">
+      <h2>Interface Admin</h2>
+      <h5>Bienvenue $nom, content de te revoir.</h5>
+    </div>
+  </div>
+
+</main>
