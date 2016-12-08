62f9269df3923dbcee55ee70277569653ec1b58f
Christophe-Junier <christophejunier@gmail.com>
2016-12-08 16:06:05 +0100

    Class donne avec envoie et affichage pour teste

--- Donnee.class.py
+++ Donnee.class.py
@@ -1,11 +1,19 @@
 #!/Python-3.5.2/
 #-*-coding:Utf-8 -*
+import requests;
 
-# initialisation du raspberry on devra integrer les differentes biblitothèque requise ici
+class Donnee: #Une class donnees
+    def __init__(self,donnees): # Notre méthode constructeur
+        self.Nom_donnee = donnees
+
+    def Envoie_donnee(adresse): # Notre méthode qui va envoie la donée (son nom) à un serveur en parametre
+        requests.post(adresse, data = self.Nom_donnee)
+
+    def Afficher_donnee(self):
+        affichage = self.Nom_donnee
+        print(affichage)
 
 
-# programme
-tant que le liens nfc n'est pas fait
-{
-    Je reessais de faire le liens
-}
+
+badge=Donnee("256")
+badge.Afficher_donnee()
