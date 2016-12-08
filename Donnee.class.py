6a279a059f8d591411d091c984ec6a2fa164afad
Christophe-Junier <christophejunier@gmail.com>
2016-12-08 15:07:12 +0100

    Début de la gestion du nfc

---
+++ Donnee.class.py
@@ -0,0 +1,11 @@
+#!/Python-3.5.2/
+#-*-coding:Utf-8 -*
+
+# initialisation du raspberry on devra integrer les differentes biblitothèque requise ici
+
+
+# programme
+tant que le liens nfc n'est pas fait
+{
+    Je reessais de faire le liens
+}
